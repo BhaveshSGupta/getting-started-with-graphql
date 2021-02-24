dcfa01a97b22d3729aa974117f12a3a7ffaea783
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2021-02-25 05:14:49 +0530

    Creating GraphQL server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const { ApolloServer, gql} = require('apollo-server')
+
+const typeDefs = gql`
+ type Query {
+  greeting: String
+ }
+`
+const resolvers = {
+	Query:{
+		greeting:()=>'Hello GraphQL world',
+	}
+}
+
+const server = new ApolloServer({ typeDefs, resolvers})
+server.listen({port: 9000})
+.then(({url})=> console.log(`Server running at ${url}`))
